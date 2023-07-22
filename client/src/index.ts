d796f41f47e41d0b525b53df739933e98b2b768d
Nikita Sinelnikov <nikita.sinelnikov@lenta.ru>
2023-07-23 01:39:59 +0300

    a

--- client/src/index.ts
+++ client/src/index.ts
@@ -1,1 +1,12 @@
+import 'normalize.css';
+import './styles.css';
+
 console.log('a');
+
+window.addEventListener('DOMContentLoaded',() => {
+    const saverCoverIMG = document.querySelector('.screen-saver') as HTMLElement;
+    const dekstopCoverIMG = document.querySelector('.dekstop') as HTMLElement;
+
+    dekstopCoverIMG.style.display = 'none';
+
+});
